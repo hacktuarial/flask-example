1aeeb2d2cf96429863abd598644fceb888d9f539
Timothy Sweetser <tsweetser@gmail.com>
2019-01-30 14:29:16 -0500

    adds REAMDE

--- tornado_sandbox/speed_test.py
+++ tornado_sandbox/speed_test.py
@@ -1,8 +1,10 @@
 """
 Sync server:
-    45.7, 50.0, 
+    45.7, 50.0,
 Async server:
     It took 57.446223 seconds to do 100 requests
+With nginx as a reverse proxy for 2 services:
+    26.5 seconds!!!
 """
 import numpy as np
 import requests
@@ -10,8 +12,13 @@ import time
 import multiprocessing as mp
 
 
+# URL = "http://localhost:8888/api/v0/house_value"
+# now i'm running nginx!
+URL = "http://localhost:80/api/v0/house_value"
+
+
 def get_prediction(i):
-    resp = requests.post("http://localhost:8888/api/v0/house_value",
+    resp = requests.post(URL,
             json={"row_index": int(i)})
     result = {**resp.json(), **{"row_index": i}}
     print(result)
